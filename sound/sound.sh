63d122afc4bb6e1467390e19976c6a3af746409d
aa62410 <36538722+aa62410@users.noreply.github.com>
2018-03-08 11:58:08 +0100

    Update sound.sh

--- sound/sound.sh
+++ sound/sound.sh
@@ -1,8 +1,9 @@
 amixer set Master on
 amixer set Speaker on
 amixer set PCM on
+amixer set Heaphone on
 
-amixer set Master 50%
 amixer set Speaker 100%
 amixer set PCM 100%
+amixer set Master 50%
 
